b20825bb8c16c687ff07acc9c44b6e7eeafbe84f
forthtilliath <vincent.lisita+gh@gmail.com>
2026-09-09 00:14:44 +0200

    chore: add prettier and tighten eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    // eslint-config-next enregistre déjà le plugin jsx-a11y : on ne fait qu'ajuster des règles.
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "jsx-a11y/no-autofocus": "warn",
+      "jsx-a11y/anchor-is-valid": "error",
+      "jsx-a11y/label-has-associated-control": "error",
+    },
+  },
+  // Override default ignores of eslint-config-next.
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+]);
+
+export default eslintConfig;
