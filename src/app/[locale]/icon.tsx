f2545c631e59fc3f5c22540ebe0014ff4271687c
forthtilliath <vincent.lisita+gh@gmail.com>
2026-09-11 00:59:08 +0200

    style: reformat icon route to prettier

--- src/app/[locale]/icon.tsx
+++ src/app/[locale]/icon.tsx
@@ -21,10 +21,7 @@ export default function Icon() {
       }}
     >
       <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M16 2s10 12.2 10 18a10 10 0 0 1-20 0C6 14.2 16 2 16 2Z"
-          fill="#d21f2c"
-        />
+        <path d="M16 2s10 12.2 10 18a10 10 0 0 1-20 0C6 14.2 16 2 16 2Z" fill="#d21f2c" />
         <ellipse cx="12.5" cy="17" rx="2.4" ry="3.4" fill="#ffffff" fillOpacity="0.35" />
       </svg>
     </div>,
